c2f2a78d601b0abe8a0f80608296437b9f79c90c
ryanbrate <r.brate@gmail.com>
2020-06-27 17:23:43 +0200

    google news dataset similar embeddings to 'smell' updated to 500

--- 0_embeddings/embeddings.py
+++ 0_embeddings/embeddings.py
@@ -1,16 +1,19 @@
-""" Output to terminal the 100 most similar words (in embedding repr) to 'smell'
-
+""" Return a .txt file with the n tokens with the greatest similarity to input word.
 Example:
-    $python3 embeddings.py
+    get the 20 most similar words to dog (in terms of embeddings)
+    $python dog 20
 """
 import gensim.downloader as api
+import sys
 
-wv = api.load('word2vec-google-news-300')
+embeddings = api.load('word2vec-google-news-300')
 
-with open("search_words.txt", "r") as f:
-    search_words = [w.strip("\n") for w in f.readlines()]
+# get ....
+returned = []
+for i in embeddings.most_similar(positive=sys.argv[1], topn=int(sys.argv[2])):
+    returned.append(i)
 
-print(search_words)
-#print the topn vectors most similar to ....
-vector = ["smell"]
-for i in wv.most_similar(positive=vector, topn=100): print(i)
+# save to file
+with open(f"results_{sys.argv[1]}_{sys.argv[2]}.txt", "w") as f:
+    for r in returned:
+        f.write(f"{r}\n")
